34e2060c8cea89f398be1e864f7ad4ffa9019239
Osagie Okoedo <okoedooc@yahoo.com>
2023-11-15 13:19:55 +0000

    Added Grafana dashboard entity card

--- app/packages/app/src/components/catalog/EntityPage.tsx
+++ app/packages/app/src/components/catalog/EntityPage.tsx
@@ -57,6 +57,9 @@ import {
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
+import {
+  EntityGrafanaDashboardsCard,
+} from '@k-phoen/backstage-plugin-grafana';
 
 const techdocsContent = (
   <EntityTechdocsContent>
@@ -130,7 +133,9 @@ const overviewContent = (
     <Grid item md={6} xs={12}>
       <EntityCatalogGraphCard variant="gridItem" height={400} />
     </Grid>
-
+    <Grid item md={6}>
+      <EntityGrafanaDashboardsCard />
+    </Grid>
     <Grid item md={4} xs={12}>
       <EntityLinksCard />
     </Grid>
